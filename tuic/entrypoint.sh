4fefc9c2ab2520bfee2e2dfb6441672e62344575
xiejiang <9955686@gmail.com>
2023-05-26 11:14:39 +0800

    update

--- tuic/entrypoint.sh
+++ tuic/entrypoint.sh
@@ -6,16 +6,21 @@ fi
 
 cat > tuic-server.json << EOF
 {
-    "port": ${SERVER_PORT},
-    "token": ["${TOKEN}"],
+    "server": "${SERVER}",
+    "users": {
+        "${UUID}": "${PASSWORD}"
+    },
     "certificate": "${CERTIFICATE}",
     "private_key": "${PRIVATE_KEY}",
-    "ip": "${IP}",
-    "congestion_controller": "${CONGESTION_CONTROLLER}",
-    "max_idle_time": ${MAX_IDLE_TIME},
-    "authentication_timeout": ${AUTHENTICATION_TIMEOUT},
-    "alpn": ["${ALPN_PROTOCOL}"],
-    "max_udp_relay_packet_size": ${MAX_UDP_RELAY_PACKET_SIZE},
+    "congestion_control": "${CONGESTION_CONTROLLER}",
+    "alpn": ["${ALPN}"],
+    "udp_relay_ipv6": ${UDP_RELAY_IPV6},
+    "zero_rtt_handshake": ${ZERO_RTT_HANDSHAKE},
+    "auth_timeout": "${AUTH_TIMEOUT}",
+    "max_idle_time": "${MAX_IDLE_TIME}",
+    "max_external_packet_size": ${MAX_EXTERNAL_PACKET_SIZE},
+    "gc_interval": "${GC_INTERVAL}",
+    "gc_lifetime": "${GC_LIFETIME}",
     "log_level": "${LOG_LEVEL}"
 }
 EOF
